69d8596aaff639fde7da2e66943cf1b781ec7309
joeseph <otoojoe230@gmail.com>
2025-03-27 19:38:18 +0100

    adding apps to the webpage

--- webpage.py
+++ webpage.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 st.set_page_config(layout="wide")
 #creating image and info column
 col1, col2=st.columns(2)
@@ -17,3 +18,14 @@ with col2:
     """
     st.info(content)
 st.write("Below you can find some of the apps I have built in Python. Feel free to contact me!")
+
+# Creating another columns for apps
+df=pd.read_csv("data.csv",sep=";")
+col3,col4=st.columns(2)
+with col3:
+    for index,row in df[0:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index,row in df[10:].iterrows():
+        st.header(row["title"])
