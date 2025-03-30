1d5d084fc0261b53a32b2011bcafd30d3bfe46ba
joeseph <otoojoe230@gmail.com>
2025-03-31 01:26:40 +0200

    send email from form

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,11 +1,19 @@
 import streamlit as st
+from sending_email import send_email
 # Adding contact me page
 st.header("Contact Me")
 
 with st.form(key="Enter_your_email"):
-    email=st.text_input("Your email address")
-    message=st.text_area("Your message")
+    user_email=st.text_input("Your email address")
+    raw_message=st.text_area("Your message")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{raw_message}
+"""
     submit_button=st.form_submit_button("Submit")
     if submit_button:
-        print(submit_button)
-        Message= message + email
+        send_email(message)
+        st.info("Your message was sent successfully")
+
--- sending_email.py
+++ sending_email.py
@@ -1,24 +1,17 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "otoojoe230@gmail.com"
-password = "qgkvdymmfzchkuui"
-receiver = "otoojoe230@gmail.com"
-message = """"\
-Subject: Appreciation!
-Hi my world!,
-Bebe girl, I would like to tell you that, I really love you.
-I am grateful to God for having you in my life.
-With you my life is perfect. 
-You always gave me joy and peace.
-Thank you for loving me babe girl.
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "otoojoe230@gmail.com"
+    password = "qgkvdymmfzchkuui"
+    receiver = "otoojoe230@gmail.com"
 
-Best regard,
-Your love and husband
-"""
-my_context = ssl.create_default_context()
-with smtplib.SMTP_SSL(host, port, context=my_context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    my_context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
+
